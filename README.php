7985748a5d25baace600d59798dc5d9f117f3623
Tanzina Akther <shitultanzina@gmail.com>
2024-02-26 20:16:02 +0600

    Update README.php

--- README.php
+++ README.php
@@ -1,17 +1,6 @@
-##    PHP EXAMPLE PROJECT    ##
-
-###  `--------Online Meal Management System--------` ###
+###  `-----Online Meal Management System-----` ###
 
 Online based meal management system.  The purpose of this project is to reduce the boring calculation and other parts of meal controlling system.  
-
-Here, most of the calculation system comes from Rajshahi region. 
-
-### Installation ###
-* `git clone https://github.com/Jaggesher/MealManagement.git`
-* `Coppy It to C:\xampp\htdocs`
-* `Create Database Named 'mealmanagement'`
-* `Copy And Execute All SQL Command on 'MealManagementDB\BasicDB.sql'`
-* `ENJOY`
 
 
 ### Used Technology  ###
@@ -24,4 +13,3 @@ Here, most of the calculation system comes from Rajshahi region.
 * `MySQL`
 
 
-### `Feel free to modify if you want.`
