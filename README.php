d2cd1b06f4617887aaed0f24e443dd72fee2af7f
Tanzina Akther <shitultanzina@gmail.com>
2024-10-16 23:55:42 +0600

    Update README.php

--- README.php
+++ README.php
@@ -1,4 +1,11 @@
 ###  `-----Tidy Meal Master ----` ###
 
-This project entitled “Tidy Meal Master” is a dynamic web-based meal management platform to manage hostel or mess calculate daily meal rate, deposit money, total cost, extra cost, extra and due money. There is a large number of hostels and mess in our country. The web platform will be great relief to the hostel or mess Manager. This platform will help Manager in case of managing hostel or mess maintaining reports. It will calculate all the complex meal system and handle previous dues and advance payments for generating current month meal and fixed and extra money. Manager can handle all the process so easily. 
-For frontend designing we use HTML, CSS and Bootstrap. For backend we use PHP & connect to MySQL for storing the data. We also used client side scripting language JavaScript to make it more user friendly.
+This project entitled “Tidy Meal Master” is a dynamic web-based meal management platform to manage hostel 
+or mess calculate daily meal rate, deposit money, total cost,extra cost, extra and due money. There is a
+large number of hostels and mess in our country.The web platform will be great relief to the hostel or mess
+Manager.This platform will help Manager in case of managing hostel or mess maintaining reports.
+It will calculate all the complex meal system and handle previous dues and advance payments
+for generating current month meal and fixed and extra money.
+Manager can handle all the process so easily.For frontend designing we use HTML, CSS and Bootstrap.
+For backend we use PHP & connect to MySQL for storing the data.We also used client side scripting language
+JavaScript to make it more user friendly.
